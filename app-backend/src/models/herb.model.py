c1e73a836e715bed71b740853e7690c17ef64516
somelamedude-git <cozydeveloper843@gmail.com>
2025-10-18 01:24:22 +0530

    many to many relationship established

--- app-backend/src/models/herb.model.py
+++ app-backend/src/models/herb.model.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, BigInteger, Boolean, CheckConstraint
+from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 class Herb(Base):
     __tablename__ = "Herb"
     Herb_Name = Column(String, unique=True)
-    Herb_ID = Column(String, primary_key=True)
+    Herb_ID : Mapped[int] = mapped_column(primary_key=True)
     Hydro_Tannins = Column(Boolean, default=False) # This means greenish-black, blue-black is given out in the test
     Condensed_Tannins = Column(Boolean, default=False) # This means the color green is given out in fecl3 test
     pH = Column(Float, CheckConstraint('pH>=0 AND pH<=14'))
--- app-backend/src/models/organisation.model.py
+++ app-backend/src/models/organisation.model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, CheckConstraint, Table, ForeignKey
 from database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 organisation_herbs = Table(
         "organisation_herbs",
@@ -10,6 +11,9 @@ organisation_herbs = Table(
 
 class Organisation(Base):
     __tablename__ = "Organisation"
-    organisation_id = Column(Integer, primary_key=True)
+    organisation_id:Mapped[int] = mapped_column(primary_key=True)
     organisation_name = Column(String)
+    herbs:Mapped[list[Herb]] = relationship(secondary=organisation_herbs)
+
+
 
